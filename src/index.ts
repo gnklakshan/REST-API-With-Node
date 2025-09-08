c4c353c0809dee54194a55ad2522e803171df62e
gnklakshan <132909147+gnklakshan@users.noreply.github.com>
2025-09-08 21:59:08 +0530

    configure mongodb

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,11 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
+import mongoose from 'mongoose';
 
 const app = express();
 const port = process.env.PORT || 3000;
+const MongoURL = process.env.MONGO_URL || 'mongodb+srv://artworkshop690:WvydN6SCmUKlAzeT@testing.rv7m8r3.mongodb.net/?retryWrites=true&w=majority&appName=testing';
 
 app.use(cors({
     credentials: true,
@@ -20,4 +22,10 @@ const server = http.createServer(app);
 
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
+});
+
+mongoose.Promise = Promise;
+mongoose.connect(MongoURL);
+mongoose.connection.on('error', (error:Error) => {
+    console.error(`MongoDB connection error: ${error}`);
 });
